586ea0a52dc22a96fc967de6fbf7272348e04df3
CURSEINME <brff282@gmail.com>
2025-02-04 10:16:07 +0300

    add feedback form logic

--- src/components/forms/FeedbackForm.tsx
+++ src/components/forms/FeedbackForm.tsx
@@ -1,60 +1,144 @@
 'use client'
 
-import { useForm } from 'react-hook-form'
-
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import Input, { isValidPhoneNumber } from 'react-phone-number-input/input'
+import 'react-phone-number-input/style.css'
 import Button from '../Button'
+
+interface IFormState {
+	name: string
+	tel: string
+	email: string
+}
 
 const FeedbackForm = () => {
 	const {
 		register,
 		handleSubmit,
-		formState: { errors }
-	} = useForm()
 
-	function onSubmit() {}
+		control,
+		formState: { errors }
+	} = useForm<IFormState>()
+
+	const [message, setMessage] = useState<string>()
+	const [error, setError] = useState<string>()
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+
+	async function onSubmit(data: IFormState) {
+		try {
+			setIsLoading(true)
+			const res = await fetch('/api/feedback', {
+				method: 'POST',
+				body: JSON.stringify(data)
+			})
+
+			if (res.ok) {
+				setMessage('Ваша заявка отправлена')
+			}
+			if (!res.ok) {
+				setError('Произошла ошибка')
+			}
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setIsLoading(false)
+		}
+	}
 
 	return (
 		<form
 			className='flex flex-col items-center'
 			onSubmit={handleSubmit(onSubmit)}
 		>
-			<div className='flex w-full flex-col gap-20 lg:flex-row'>
-				<div className='flex w-full flex-col md:flex-row md:justify-between lg:flex-col'>
-					<label className='mb-2 text-lg' htmlFor='name'>
+			<div className='flex w-full flex-col gap-10 sm:gap-20 lg:flex-row'>
+				<div className='flex items-center justify-between gap-10'>
+					<label className='mb-7 text-lg' htmlFor='name-field'>
 						Имя
 					</label>
-					<input
-						{...register('name')}
-						className='w-full rounded-sm px-10 py-3 text-black md:w-[500px] lg:w-full'
-						type='name'
-						placeholder='Имя'
-					></input>
+					<div>
+						<input
+							{...register('name', {
+								required: 'Обязательное поле'
+							})}
+							className={`w-[200px] border-4 ${errors.name && 'border-red-500'} rounded-sm px-5 py-3 text-black focus:outline-none sm:w-[500px] lg:w-full`}
+							type='name'
+							placeholder='Имя'
+							autoComplete='on'
+							id='name-field'
+						></input>
+						<div className='mt-2 h-[20px] text-sm font-bold text-red-500'>
+							{errors.name?.message}
+						</div>
+					</div>
 				</div>
-				<div className='flex w-full flex-col md:flex-row md:justify-between lg:flex-col'>
-					<label className='mb-2 text-lg' htmlFor='tel'>
+				<div className='flex items-center justify-between gap-10'>
+					<label className='mb-7 text-lg' htmlFor='tel-field'>
 						Телефон
 					</label>
-					<input
-						{...register('tel')}
-						className='w-full rounded-sm px-10 py-3 text-black md:w-[500px] lg:w-full'
-						type='tel'
-						placeholder='Телефон'
-					></input>
+					<div>
+						<Controller
+							name='tel'
+							control={control}
+							rules={{
+								required: 'Обязательное поле',
+								validate: value =>
+									isValidPhoneNumber(value) ? true : 'Неверный формат телефона'
+							}}
+							render={({ field: { onChange, value } }) => (
+								<Input
+									className={`w-[200px] border-4 ${errors.tel && 'border-red-500'} rounded-sm px-5 py-3 text-black focus:outline-none sm:w-[500px] lg:w-full`}
+									country='RU'
+									value={value}
+									withCountryCallingCode
+									international
+									onChange={onChange}
+									id='tel-field'
+								/>
+							)}
+						/>
+						<div className='mt-2 h-[20px] text-sm font-bold text-red-500'>
+							{errors.tel?.message}
+						</div>
+					</div>
 				</div>
-				<div className='flex w-full flex-col md:flex-row md:justify-between lg:flex-col'>
-					<label className='mb-2 text-lg' htmlFor='email'>
+				<div className='flex items-center justify-between gap-10'>
+					<label className='mb-7 text-lg' htmlFor='email-field'>
 						Email
 					</label>
-					<input
-						{...register('email')}
-						className='w-full rounded-sm px-10 py-3 text-black md:w-[500px] lg:w-full'
-						type='email'
-						placeholder='Email'
-					></input>
+					<div className='flex flex-col'>
+						<input
+							{...register('email', {
+								required: 'Обязательное поле'
+							})}
+							className={`w-[200px] border-4 ${errors.email && 'border-red-500'} rounded-sm px-5 py-3 text-black focus:outline-none sm:w-[500px] lg:w-full`}
+							type='email'
+							placeholder='Email'
+							autoComplete='on'
+							id='email-field'
+						></input>
+						<div className='mt-2 h-[20px] text-sm font-bold text-red-500'>
+							{errors.email?.message}
+						</div>
+					</div>
 				</div>
 			</div>
-			<Button type='submit' className='mt-10 px-2 py-3 text-xl'>
-				Отправить
+			{message && (
+				<div className='mt-5 h-[20px] text-lg font-bold text-green-500'>
+					{message}
+				</div>
+			)}
+			{error && (
+				<div className='mt-5 h-[20px] text-lg font-bold text-red-500'>
+					{error}
+				</div>
+			)}
+			<Button
+				type='submit'
+				disabled={isLoading}
+				className='mt-10 px-2 py-3 text-xl'
+			>
+				{isLoading ? 'Отправка...' : 'Отправить'}
 			</Button>
 		</form>
 	)
